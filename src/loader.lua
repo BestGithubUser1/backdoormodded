27bafd1e3b54b4dcfe206402baf21dbbb7fdd692
BestGithubUser1 <132391268+BestGithubUser1@users.noreply.github.com>
2023-05-02 21:22:19 +0300

    Update loader.lua

--- src/loader.lua
+++ src/loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/iK4oS/backdoor.exe/v8/src/main.lua"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BestGithubUser1/backdoormodded/main/src/main.lua"))();
