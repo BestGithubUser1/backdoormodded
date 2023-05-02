1193735aeb25945c09fe90b2c143b21b03d887b7
BestGithubUser1 <132391268+BestGithubUser1@users.noreply.github.com>
2023-05-02 22:10:14 +0300

    Update main.lua

--- src/main.lua
+++ src/main.lua
@@ -44,7 +44,7 @@ if genv.backdoorexe then
 end
 
 --// UI \\--
-local screenGui, uiRequire = loadstring(game:HttpGet("https://raw.githubusercontent.com/iK4oS/backdoor.exe/v8/src/ui.lua"))()
+local screenGui, uiRequire = loadstring(game:HttpGet("https://raw.githubusercontent.com/BestGithubUser1/backdoormodded/main/src/ui.lua"))()
 local alertLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/uniquadev/GuiToLuaConverter/main/PluginPlace/src/alerts.lua"))()
 
 local ui = uiRequire(screenGui.main);
